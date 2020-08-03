70e0f4cadffdee11033c5e12a2bac338c45e452e
johndoe2196 <johndoe219656@gmail.com>
2020-08-03 17:08:55 +0100

    added apiKey field to mongodb

--- model/User.js
+++ model/User.js
@@ -22,6 +22,10 @@ const userSchema = new mongoose.Schema({
     date: {
         type: Date,
         default: Date.now
+    },
+    apiKey: {
+        type: String,
+        default: null
     }
 });
 
